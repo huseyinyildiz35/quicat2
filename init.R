b5c7df8ae0834ec8820de0ac7b889adaf3427c06
Huseyin.Yildiz <52738594+huseyinyildiz35@users.noreply.github.com>
2024-01-10 00:04:59 +1100

    Update init.R

--- init.R
+++ init.R
@@ -1,10 +1,12 @@
 
 
-my_packages = c("shiny", "shinydashboard","readxl","shinythemes","catR","shinyBS","shinysjs","dplyr","shinydashboardPlus","ggplot2","DBI","dbplyr","RMySQL","writexl","shinyalert","shinyhelper")
+my_packages = c("shiny", "shinydashboard","readxl","shinythemes","catR","shinyBS","shinyjs","dplyr","shinydashboardPlus","ggplot2","DBI","dbplyr","RMySQL","writexl","shinyalert","shinyhelper")
+
+my_packages = c("package_name_1", "package_name_2", ...)
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
+    install.packages(p, clean=TRUE, quiet=TRUE)
   }
 }
 
